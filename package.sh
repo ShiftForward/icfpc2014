9cd2fc6d50e84053dd833f24a203ce39b8686b58
Andre Silva <andre.beat@gmail.com>
2014-07-28 03:57:38 +0100

    final solution

--- package.sh
+++ package.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-./lhc src/main/ghc/default.ghc > solution/ghost0.ghc
-./lhc src/main/ghc/ghost.ghc > solution/ghost1.ghc
-./lhc src/main/ghc/pledge.ghc > solution/ghost3.ghc
-./lhc src/main/ghc/pledge-reversed.ghc > solution/ghost2.ghc
+./lhc src/main/ghc/pledge.ghc > solution/ghost0.ghc
+./lhc src/main/ghc/pledge-reversed.ghc > solution/ghost1.ghc
 ./llc src/main/lambdalisp/greedy-bot.ll --bot > solution/lambdaman.gcc
