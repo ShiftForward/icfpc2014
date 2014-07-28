8dc91cd3db7b32bee0d2cb8ccb35b76d3db4bec7
Hugo Ferreira <hugo@shiftforward.eu>
2014-07-28 03:31:51 +0100

    Changed for pledge ghost.

--- package.sh
+++ package.sh
@@ -2,6 +2,6 @@
 
 ./lhc src/main/ghc/default.ghc > solution/ghost0.ghc
 ./lhc src/main/ghc/ghost.ghc > solution/ghost1.ghc
-./lhc src/main/ghc/ghost-fright.ghc > solution/ghost3.ghc
-./lhc src/main/ghc/ghost-fright-vertical.ghc > solution/ghost2.ghc
+./lhc src/main/ghc/pledge.ghc > solution/ghost3.ghc
+./lhc src/main/ghc/pledge-reversed.ghc > solution/ghost2.ghc
 ./llc src/main/lambdalisp/greedy-bot.ll --bot > solution/lambdaman.gcc
